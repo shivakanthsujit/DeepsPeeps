536dfc29afcd4a75d22384153a49c2abe3b4525b
Shiva Kanth <shivakanth.sujit@gmail.com>
2018-05-16 13:44:01 +0530

    Align commnent box

--- scripts/main.js
+++ scripts/main.js
@@ -4,9 +4,13 @@ function addMentee()
 	var newMentee = document.createElement('li');
 	var mRating = prompt("Enter a rating ( 1 - 5 ): ");
 	var mComment = document.createElement("TEXTAREA");
-	newMentee.textContent = menteeName + " Rating = " + mRating;
-	var linebreak = document.createElement('br');
+	
+    
+    newMentee.textContent = menteeName +" Rating = " + mRating;
+    var linebreak = document.createElement('br');
+	var label = document.createTextNode("Comments: ");
 	newMentee.appendChild(linebreak);
+	newMentee.appendChild(label);
 	newMentee.appendChild(mComment);
 	var mList = document.getElementById("list");
 
